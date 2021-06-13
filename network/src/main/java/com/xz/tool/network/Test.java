238d365711f44ad23107816d8fa681e3f8cc0f58
xzlyf <czr2001@outlook.com>
2021-06-13 13:38:39 +0800

    commit

---
+++ network/src/main/java/com/xz/tool/network/Test.java
@@ -0,0 +1,12 @@
+package com.xz.tool.network;
+
+/**
+ * @author czr
+ * @email czr2001@outlook.com
+ * @date 2021/6/13
+ */
+public class Test {
+	public static long test() {
+		return System.currentTimeMillis();
+	}
+}
